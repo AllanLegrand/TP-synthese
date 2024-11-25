934fabbc0edd7998b6de2aec8f4c65531530e4ed
Allan Legrand <58049420+AllanLegrand@users.noreply.github.com>
2024-11-25 16:05:03 +0100

    Create TacheModele.php

---
+++ app/Models/TacheModele.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class TacheModele extends Model
+{
+	protected $table = 'Tache';
+	protected $primaryKey = 'idTache';
+	protected $allowedFields = [
+		'titre',
+		'description',
+		'echeance',
+		'priorite',
+		'statut',
+		'dateCreation',
+		'idProjet'
+	];
+}
