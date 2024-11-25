124b66d21bf445b946b13cf473455d7287811567
Trystan B. <130407095+ydroo@users.noreply.github.com>
2024-11-25 16:33:10 +0100

    Update signin.php

--- app/Views/signin.php
+++ app/Views/signin.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Connexion</title>
+	<link href="/assets/css/connexion.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-	<h1>Connexion</h1>
 	<?php if (session()->getFlashdata('msg')): ?>
 		<div style="color: red;">
 			<?= session()->getFlashdata('msg') ?>
 		</div>
 	<?php endif; ?>
 	<form action="/SigninController/loginAuth" method="post">
+		<h3>Connexion</h3>
 		<label for="email">E-mail :</label>
-		<input type="email" name="email" id="email" required>
+		<input type="email" name="email" id="email" placeholder="Entrez votre adresse e-mail" required>
 		<br>
 		<label for="password">Mot de passe :</label>
-		<input type="password" name="password" id="password" required>
+		<input type="password" name="password" id="password" placeholder="Entrez votre mot de passe" required>
+		<br>
+		<a href="/forgot-password">Mot de passe oublié ?</a>
 		<br>
 		<button type="submit">Se connecter</button>
 	</form>
-	<a href="/forgot-password">Mot de passe oublié ?</a>
 </body>
-
 </html>
