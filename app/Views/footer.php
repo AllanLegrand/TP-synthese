a7ac854509b7966f04761247179ca99af3135ccf
Lorenzo <lorenzo.de-macedo@etu.univ-lehavre.fr>
2024-11-27 10:46:57 +0100

    modification footer et header

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,7 +1,7 @@
 </main> <!-- Fin du main -->
     <!-- Footer -->
     <footer class="bg-light text-center py-3 mt-auto">
-        <p class="mb-0">© 2024 Gestion des Tâches. Tous droits réservés.</p>
+        <p class="mb-0">© 2024 TP-Synthèse | DE MACEDO Lorenzo, LEGRAND Allan, BAILLOBAY Trystan, CHAMPVILLARD Sebastien.</p>
     </footer>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
