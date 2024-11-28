91a580f209e6beb84384d11dfe1aaa02c906cb3d
Trystan B. <130407095+ydroo@users.noreply.github.com>
2024-11-28 15:35:12 +0100

    Create tache_util.js

---
+++ public/assets/js/tache_util.js
@@ -0,0 +1,9 @@
+// Ouvrir la popup de partage
+function openSharePopup() {
+    document.getElementById('popupShare').style.display = 'flex';
+}
+
+// Fermer la popup de partage
+function closeSharePopup() {
+    document.getElementById('popupShare').style.display = 'none';
+}
